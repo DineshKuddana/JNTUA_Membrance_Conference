b73adf62d824c9e76ef82a337a2e28ee517a38da
DineshKuddana <dineshkuddana@gmail.com>
2024-09-04 16:18:24 +0530

    image updated

--- client/src/components/Registration/registration.js
+++ client/src/components/Registration/registration.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./registration.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import QRImage from '../../images/SBI_QR.jpg';
+import QRImage from '../../images/QR.png';
 
 function App() {
   const [formState, setFormState] = useState({
