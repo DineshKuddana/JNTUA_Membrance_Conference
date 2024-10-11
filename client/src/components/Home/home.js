9a62d787d9f74dfa888d47dbe970fd290e0f6cae
DineshKuddana <dineshkuddana@gmail.com>
2024-10-11 11:16:02 +0530

    Footer updated with developer admin

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import collegeImage from '../../images/jntua.webp'; 
-import collegeLogo from '../../images/logo.png'; 
-import './home.css'; 
+import React from "react";
+import collegeImage from "../../images/jntua.webp";
+import collegeLogo from "../../images/logo.png";
+import "./home.css";
 
 const Home = () => {
   return (
@@ -10,24 +10,39 @@ const Home = () => {
       <section className="conference-details">
         <h1 className="conference-title">International Conference on</h1>
         <h2 className="conference-subtitle">
-          “Membrane Processes for a Green Environment: Current Status, Challenges, and Future Prospects”
+          “Membrane Processes for a Green Environment: Current Status,
+          Challenges, and Future Prospects”
         </h2>
         <p className="conference-date">16 - 18 October 2024</p>
-        <p className="conference-location">Ananthapuramu, Andhra Pradesh, India</p>
+        <p className="conference-location">
+          Ananthapuramu, Andhra Pradesh, India
+        </p>
       </section>
 
       {/* About the Conference Section */}
       <section className="about-conference">
         <h3 className="section-title">About the Conference:</h3>
         <p>
-          The Indian Membrane Society organizes this conference annually to commemorate the Birth Anniversary of Prof. S. Sourirajan, a pioneer in Phase Inversion Membranes. This year, the event will take place at JNTUA, Anantapur, focusing on “Membrane Processes for a Green Environment: Current Status, Challenges, and Future Prospects.” The conference will offer a comprehensive look into membrane technologies and their role in sustainable development.
+          The Indian Membrane Society organizes this conference annually to
+          commemorate the Birth Anniversary of Prof. S. Sourirajan, a pioneer in
+          Phase Inversion Membranes. This year, the event will take place at
+          JNTUA, Anantapur, focusing on “Membrane Processes for a Green
+          Environment: Current Status, Challenges, and Future Prospects.” The
+          conference will offer a comprehensive look into membrane technologies
+          and their role in sustainable development.
         </p>
       </section>
 
       {/* College Image and Download Button Section */}
       <section className="image-section">
         <img src={collegeImage} alt="College" className="college-image" />
-        <a href="https://drive.google.com/file/d/12Vg7hGdVfyxxXUnNaw8Sp0UE3UL9RXzk/view?usp=sharing" target='_blank'  rel = "noreferrer" className="download-button" download>
+        <a
+          href="https://drive.google.com/file/d/12Vg7hGdVfyxxXUnNaw8Sp0UE3UL9RXzk/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          className="download-button"
+          download
+        >
           Download Brochure
         </a>
       </section>
@@ -58,7 +73,9 @@ const Home = () => {
           </div>
           <div className="topic">
             <h4>Biological Inventions</h4>
-            <p>Biological inventions and technologies for a green environment.</p>
+            <p>
+              Biological inventions and technologies for a green environment.
+            </p>
           </div>
           <div className="topic">
             <h4>Clean Technologies</h4>
@@ -66,7 +83,10 @@ const Home = () => {
           </div>
           <div className="topic">
             <h4>Bio-Membranes</h4>
-            <p>Bio-membranes, nanomaterials, nano composites, and nano-technology.</p>
+            <p>
+              Bio-membranes, nanomaterials, nano composites, and
+              nano-technology.
+            </p>
           </div>
           <div className="topic">
             <h4>Desalination</h4>
@@ -74,7 +94,9 @@ const Home = () => {
           </div>
           <div className="topic">
             <h4>Membrane Modification</h4>
-            <p>Membrane modification and characterization for green environment.</p>
+            <p>
+              Membrane modification and characterization for green environment.
+            </p>
           </div>
           <div className="topic">
             <h4>Molecular Dynamics</h4>
@@ -117,28 +139,59 @@ const Home = () => {
           <div className="organizer-card">
             <h4>Indian Membrane Society</h4>
             <p>Baroda, India</p>
-            <a href="http://www.indianmembranesociety.org" target="_blank" rel="noopener noreferrer">
+            <a
+              href="http://www.indianmembranesociety.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               www.indianmembranesociety.org
             </a>
           </div>
           <div className="organizer-card">
-            <h4>Department of Chemical Engineering</h4><br></br>
-            <p>JNTUA College of Engineering <br></br>Ananthapuramu, India</p>
-            <a href="https://www.jntuacea.ac.in/" target="_blank" rel="noopener noreferrer">
-            https://www.jntuacea.ac.in/
+            <h4>Department of Chemical Engineering</h4>
+            <br></br>
+            <p>
+              JNTUA College of Engineering <br></br>Ananthapuramu, India
+            </p>
+            <a
+              href="https://www.jntuacea.ac.in/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://www.jntuacea.ac.in/
             </a>
           </div>
 
           <div className="address-card">
-            <img src={collegeLogo} alt="College Logo" className="college-logo" />
-            <p>JNTUA College of Engineering Ananthapuramu,<br></br>Andhra Pradesh, India - 515002</p>
+            <img
+              src={collegeLogo}
+              alt="College Logo"
+              className="college-logo"
+            />
+            <p>
+              JNTUA College of Engineering Ananthapuramu,<br></br>Andhra
+              Pradesh, India - 515002
+            </p>
           </div>
         </div>
       </section>
 
       {/* Footer Section */}
       <footer className="footer">
-        <p>&copy; {new Date().getFullYear()} JNTUA College of Engineering. All rights reserved.</p>
+        <p>
+          &copy; {new Date().getFullYear()} JNTUA College of Engineering. All
+          rights reserved.
+        </p>
+        <p className="developer-info">
+          Developed with <span className="heart">&hearts;</span> by{" "}
+          <a
+            href="https://www.linkedin.com/in/dinesh-kuddana-198a0a243/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dinesh Kuddana
+          </a>
+        </p>
       </footer>
     </div>
   );
